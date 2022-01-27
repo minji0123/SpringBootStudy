2ae7ab769cc716be8446741c5eb7d4250eb7ecef
minji0123 <sso02027@naver.com>
2022-01-27 17:14:24 +0900

    REST

---
+++ firstProject/src/main/java/com/example/firstProject/api/FirstApiController.java
@@ -0,0 +1,4 @@
+package com.example.firstProject.api;
+
+public class FirstApiController {
+}
--- firstProject/src/main/java/com/example/firstProject/entity/Article.java
+++ firstProject/src/main/java/com/example/firstProject/entity/Article.java
@@ -5,12 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity // 이 어노테이션이 있어야 DB 가 해당 객체를 인식 가능
+@Entity // 이 어노테이션이 있어야 DB 가 해당 객체를 인식 가능 => 해당 클래스Article 로 테이블을 만든다.
 @AllArgsConstructor // constructor 롬복
 @NoArgsConstructor // 디폴트 constructor 롬북
 @ToString // toString 롬복
@@ -18,7 +15,7 @@ import javax.persistence.Id;
 public class Article {
 
     @Id // id 삽입!
-    @GeneratedValue // 자동으로 id 넘버링
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동으로 id 넘버링 + db가 id를 자동으로 생성해줌
     private Long id; // DB 에는 대표값을 넣어줘야 한다. id 키값같은거
 
     @Column // 이 어노테이션이 있어야 DB 에서 컬럼으로 인식 가능
